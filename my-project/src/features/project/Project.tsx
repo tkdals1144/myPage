ececfa94880c99c36950c477861cf9a99f27a54c
tkdals1144 <146908349+tkdals1144@users.noreply.github.com>
2026-01-11 04:54:13 +0900

    update

--- my-project/src/features/project/Project.tsx
+++ my-project/src/features/project/Project.tsx
@@ -1,25 +1,53 @@
 import { useState } from "react";
 import styles from "./Project.module.css";
+import { PROJECT_DATA } from "./data";
 
 const Project = () => {
-    const [project, setProject] = useState("project1");
+    // 값 자체를 타입으로 승격시키는 방법 (as const)
+    const PROJECT_KEYS = ["project1", "project2"] as const;
+    type ProjectKey = (typeof PROJECT_KEYS)[number];
+    const [project, setProject] = useState<ProjectKey>("project1");
+    const data = PROJECT_DATA[project];
+
     return (
         <div className={styles.project_wrap_wrap}>
             <div className={styles.porject_wrap}>
                 <ul className={styles.project_type}>
-                    {[1, 2].map((i) => (
+                    {PROJECT_KEYS.map((key) => (
                         <li
                             className={`${styles.project_num} ${
-                                project === `project${i}` ? styles.project_num_active : styles.project_num_invalid
+                                project === key ? styles.project_num_active : styles.project_num_invalid
                             }`}
-                            onClick={() => setProject(`project${i}`)}
+                            onClick={() => setProject(key)}
                         >
-                            project{i}
+                            {key}
                         </li>
                     ))}
                 </ul>
+                <div className={styles.file_wrap}>
+                    <a href={data.url} className={styles.file} download>
+                        {project} portfolio
+                    </a>
+                </div>
                 <div className={styles.project_img_wrap}>
-                    <img className={styles.img}></img>
+                    <img className={styles.project_img} src={`${import.meta.env.BASE_URL}/images/${project}.png`}></img>
+                </div>
+                <div className={styles.title_wrap}>
+                    <h1 className={styles.title}>{data.title}</h1>
+                    <h4 className={styles.outline}>( {data.outline} )</h4>
+                </div>
+                <hr />
+                <p className={styles.explain}>{data.explain}</p>
+                <div className={styles.skill_wrap}>
+                    <h1 className={styles.title} style={{ fontWeight: "400" }}>
+                        사용 기술
+                    </h1>
+                    <hr />
+                    <ul className={styles.skill_ul}>
+                        {data.skills.map((skill) => (
+                            <li className={styles.skill_li}>{skill}</li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </div>
---
+++ my-project/src/features/project/data.ts
@@ -0,0 +1,16 @@
+export const PROJECT_DATA = {
+    project1: {
+        title: "CALTIZM",
+        outline: "온라인 쇼핑몰 프로젝트",
+        explain: "데이터 크롤링, 상품 조회 및 필터링, 로그인, 마이페이지, 게시판 등의 기능을 구현한 쇼핑몰 프로젝트",
+        skills: ["HTML5 (thymeleaf), CSS, Vanilla JS", "SpringBoot", "mySQL"],
+        url: `${import.meta.env.BASE_URL}/files/project1.pdf`,
+    },
+    project2: {
+        title: "CATIZM SPA 프로젝트",
+        outline: "CALTIZM 프로젝트 아키텍처 리팩터링",
+        explain: "Thymeleaf 템플릿 엔진을 이용한 SSR 구조를 React js 라이브버리를 통해 CSR 구조로 전환한 프로젝트",
+        skills: ["HTML5, CSS, React JS", "SpringBoot", "mySQL"],
+        url: `${import.meta.env.BASE_URL}/files/project2.pdf`,
+    },
+};
